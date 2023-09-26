15f473aec88b418ceb0762df1545be9fa41cfc63
Richardge98 <richardge1998@hotmail.com>
2023-09-27 00:41:23 +0100

    增大了界面，同时给数据页面增加了滚动条

--- dev/main.js
+++ dev/main.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const moment = require('moment');
 
 // System requirements
-const { app, BrowserWindow, ipcMain, desktopCapturer, dialog } = require('electron');
+const { app, BrowserWindow, ipcMain, desktopCapturer, dialog, webFrame } = require('electron');
 const electronLocalshortcut = require('electron-localshortcut');
 
 // importing modules
@@ -21,246 +21,236 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 let mainWindow;
 
 function createMainWindow() {
-    mainWindow = new BrowserWindow({
-        width: 550,
-        height: 870,
-        resizable: false,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            devTools: isDev,
-        },
-    });
-    if (process.platform == 'darwin') {
-        mainWindow.loadFile('./dev/public/mac-index.html');
-    } else if (process.platform == 'win32') {
-        mainWindow.loadFile('./dev/public/win-index.html');
-    }
-    electronLocalshortcut.register(mainWindow, 'Cmd+S', () => {
-        mainWindow.webContents.send('save current panju from shortcut');
-    });
-    electronLocalshortcut.register(mainWindow, 'Ctrl+S', () => {
-        mainWindow.webContents.send('save current panju from shortcut');
-    });
-    electronLocalshortcut.register(mainWindow, 'Cmd+[', () => {
-        mainWindow.webContents.send('back to home page');
-    });
-    electronLocalshortcut.register(mainWindow, 'Ctrl+[', () => {
-        mainWindow.webContents.send('back to home page');
-    });
-    // electronLocalshortcut.register(mainWindow, 'Cmd+r', () => {
-    //     mainWindow.webContents.send('refresh current time');
-    // });
-    // electronLocalshortcut.register(mainWindow, 'Ctrl+r', () => {
-    //     mainWindow.webContents.send('refresh current time');
-    // });
+  mainWindow = new BrowserWindow({
+    // width: 550,
+    // height: 870,
+    width: 600,
+    height: 940,
+    resizable: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: isDev,
+    },
+  });
+  if (process.platform == 'darwin') {
+    mainWindow.loadFile('./dev/public/mac-index.html');
+  } else if (process.platform == 'win32') {
+    mainWindow.loadFile('./dev/public/win-index.html');
+  }
+  electronLocalshortcut.register(mainWindow, 'Cmd+S', () => {
+    mainWindow.webContents.send('save current panju from shortcut');
+  });
+  electronLocalshortcut.register(mainWindow, 'Ctrl+S', () => {
+    mainWindow.webContents.send('save current panju from shortcut');
+  });
+  electronLocalshortcut.register(mainWindow, 'Cmd+[', () => {
+    mainWindow.webContents.send('back to home page');
+  });
+  electronLocalshortcut.register(mainWindow, 'Ctrl+[', () => {
+    mainWindow.webContents.send('back to home page');
+  });
 }
 
 app.on('ready', () => {
-    createMainWindow();
-    mainWindow.on('ready', () => (mainWindow = null));
+  createMainWindow();
+  mainWindow.on('ready', () => (mainWindow = null));
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createMainWindow();
-    }
+  if (mainWindow === null) {
+    createMainWindow();
+  }
 });
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    } else {
-        electronLocalshortcut.unregister(mainWindow, 'Cmd+S');
-        electronLocalshortcut.unregister(mainWindow, 'Ctrl+S');
-        electronLocalshortcut.unregister(mainWindow, 'Cmd+[');
-        electronLocalshortcut.unregister(mainWindow, 'Ctrl+[');
-        // electronLocalshortcut.unregister(mainWindow, 'Cmd+r');
-        // electronLocalshortcut.unregister(mainWindow, 'Ctrl+r');
-        mainWindow = null;
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  } else {
+    electronLocalshortcut.unregister(mainWindow, 'Cmd+S');
+    electronLocalshortcut.unregister(mainWindow, 'Ctrl+S');
+    electronLocalshortcut.unregister(mainWindow, 'Cmd+[');
+    electronLocalshortcut.unregister(mainWindow, 'Ctrl+[');
+    // electronLocalshortcut.unregister(mainWindow, 'Cmd+r');
+    // electronLocalshortcut.unregister(mainWindow, 'Ctrl+r');
+    mainWindow = null;
+  }
 });
 
 ipcMain.on('正时起局', (e, data) => {
-    currentPanjuData = data;
-    const time = timeInfo(data.timeInfo.date, data.timeInfo.hour, false);
-    if (data.paipanMethod == '飞盘') {
-        const paipan = paiFeiPan(time.jieqi, time.ri, time.shi);
-        const paiPanResult = {
-            time: time,
-            paipan: paipan,
-            hour: data.timeInfo.hour,
-        };
-        e.reply('current panju data', data);
-        e.reply('飞盘排盘', paiPanResult);
-    } else if (data.paipanMethod == '转盘') {
-        const paipan = paiZhuanPan(time.jieqi, time.ri, time.shi);
-        const paiPanResult = {
-            time: time,
-            paipan: paipan,
-            hour: data.timeInfo.hour,
-        };
-        e.reply('current panju data', data);
-        e.reply('传统转盘排盘', paiPanResult);
-    } else if (data.paipanMethod == '星飞门转') {
-        const paipan = xingfeiMenzhuan(time.jieqi, time.ri, time.shi);
-        const paiPanResult = {
-            time: time,
-            paipan: paipan,
-            hour: data.timeInfo.hour,
-        };
-        e.reply('current panju data', data);
-        e.reply('星飞门转', paiPanResult);
-    }
+  currentPanjuData = data;
+  const time = timeInfo(data.timeInfo.date, data.timeInfo.hour, false);
+  if (data.paipanMethod == '飞盘') {
+    const paipan = paiFeiPan(time.jieqi, time.ri, time.shi);
+    const paiPanResult = {
+      time: time,
+      paipan: paipan,
+      hour: data.timeInfo.hour,
+    };
+    e.reply('current panju data', data);
+    e.reply('飞盘排盘', paiPanResult);
+  } else if (data.paipanMethod == '转盘') {
+    const paipan = paiZhuanPan(time.jieqi, time.ri, time.shi);
+    const paiPanResult = {
+      time: time,
+      paipan: paipan,
+      hour: data.timeInfo.hour,
+    };
+    e.reply('current panju data', data);
+    e.reply('传统转盘排盘', paiPanResult);
+  } else if (data.paipanMethod == '星飞门转') {
+    const paipan = xingfeiMenzhuan(time.jieqi, time.ri, time.shi);
+    const paiPanResult = {
+      time: time,
+      paipan: paipan,
+      hour: data.timeInfo.hour,
+    };
+    e.reply('current panju data', data);
+    e.reply('星飞门转', paiPanResult);
+  }
 });
 
 ipcMain.on('报数起局', (e, data) => {
-    currentPanjuData = data;
-    const time = timeInfo(
-        data.timeInfo.date,
-        data.timeInfo.hour,
-        data.choosenMethod,
-        data.choosenNumber,
+  currentPanjuData = data;
+  const time = timeInfo(
+    data.timeInfo.date,
+    data.timeInfo.hour,
+    data.choosenMethod,
+    data.choosenNumber,
+  );
+  if (data.paipanMethod == '飞盘') {
+    const paipan = paiFeiPan(time.jieqi, time.ri, time.shi, data.choosenNumber, data.choosenMethod);
+    const paiPanResult = {
+      time: time,
+      paipan: paipan,
+      hour: data.timeInfo.hour,
+    };
+    e.reply('current panju data', data);
+    e.reply('飞盘排盘', paiPanResult);
+  } else if (data.paipanMethod == '转盘') {
+    const paipan = paiZhuanPan(
+      time.jieqi,
+      time.ri,
+      time.shi,
+      data.choosenNumber,
+      data.choosenMethod,
     );
-    if (data.paipanMethod == '飞盘') {
-        const paipan = paiFeiPan(
-            time.jieqi,
-            time.ri,
-            time.shi,
-            data.choosenNumber,
-            data.choosenMethod,
-        );
-        const paiPanResult = {
-            time: time,
-            paipan: paipan,
-            hour: data.timeInfo.hour,
-        };
-        e.reply('current panju data', data);
-        e.reply('飞盘排盘', paiPanResult);
-    } else if (data.paipanMethod == '转盘') {
-        const paipan = paiZhuanPan(
-            time.jieqi,
-            time.ri,
-            time.shi,
-            data.choosenNumber,
-            data.choosenMethod,
-        );
-        const paiPanResult = {
-            time: time,
-            paipan: paipan,
-            hour: data.timeInfo.hour,
-        };
-        e.reply('current panju data', data);
-        e.reply('传统转盘排盘', paiPanResult);
-    } else if (data.paipanMethod == '星飞门转') {
-        const paipan = xingfeiMenzhuan(
-            time.jieqi,
-            time.ri,
-            time.shi,
-            data.choosenNumber,
-            data.choosenMethod,
-        );
-        const paiPanResult = {
-            time: time,
-            paipan: paipan,
-            hour: data.timeInfo.hour,
-        };
-        e.reply('current panju data', data);
-        e.reply('星飞门转', paiPanResult);
-    }
+    const paiPanResult = {
+      time: time,
+      paipan: paipan,
+      hour: data.timeInfo.hour,
+    };
+    e.reply('current panju data', data);
+    e.reply('传统转盘排盘', paiPanResult);
+  } else if (data.paipanMethod == '星飞门转') {
+    const paipan = xingfeiMenzhuan(
+      time.jieqi,
+      time.ri,
+      time.shi,
+      data.choosenNumber,
+      data.choosenMethod,
+    );
+    const paiPanResult = {
+      time: time,
+      paipan: paipan,
+      hour: data.timeInfo.hour,
+    };
+    e.reply('current panju data', data);
+    e.reply('星飞门转', paiPanResult);
+  }
 });
 
 ipcMain.on('screenshot', (e, data) => {
-    desktopCapturer
-        .getSources({
-            types: ['window'],
-            thumbnailSize: { width: 8000, height: 8000 },
-        })
-        .then((sources) => {
-            for (const source of sources) {
-                if (true) {
-                    // Create a file name based on the current date
-                    const fileName = getFileName(data);
+  desktopCapturer
+    .getSources({
+      types: ['window'],
+      thumbnailSize: { width: 8000, height: 8000 },
+    })
+    .then((sources) => {
+      for (const source of sources) {
+        if (true) {
+          // Create a file name based on the current date
+          const fileName = getFileName(data);
 
-                    // Save the screenshot as a JPG file on the desktop
-                    const filePath = path.join(app.getPath('desktop'), `${fileName}.jpg`);
-                    fs.writeFile(filePath, source.thumbnail.toJPEG(100), (error) => {
-                        if (error) {
-                            console.error('Failed to save screenshot:', error);
-                        }
-                    });
-                    break;
-                }
+          // Save the screenshot as a JPG file on the desktop
+          const filePath = path.join(app.getPath('desktop'), `${fileName}.jpg`);
+          fs.writeFile(filePath, source.thumbnail.toJPEG(100), (error) => {
+            if (error) {
+              console.error('Failed to save screenshot:', error);
             }
-        })
-        .catch((error) => {
-            // console.error('Failed to capture screen:', error);
-        });
+          });
+          break;
+        }
+      }
+    })
+    .catch((error) => {
+      // console.error('Failed to capture screen:', error);
+    });
 });
 
 function getFileName(customFileName) {
-    if (customFileName != 'none') {
-        if (customFileName.includes('\n')) {
-            for (let i = 0; i < customFileName.length; i++) {
-                if (customFileName[i] == '\n') {
-                    customFileName = customFileName.slice(0, i);
-                    return customFileName;
-                }
-            }
-        } else {
-            return customFileName;
+  if (customFileName != 'none') {
+    if (customFileName.includes('\n')) {
+      for (let i = 0; i < customFileName.length; i++) {
+        if (customFileName[i] == '\n') {
+          customFileName = customFileName.slice(0, i);
+          return customFileName;
         }
+      }
     } else {
-        const date = moment().format('YYYY-MM-DD_HH-mm-ss');
-        return `奇门预测截图_${date}`;
+      return customFileName;
     }
+  } else {
+    const date = moment().format('YYYY-MM-DD_HH-mm-ss');
+    return `奇门预测截图_${date}`;
+  }
 }
 
 ipcMain.on('export saved data', (e, data) => {
-    data = JSON.stringify(data);
-    dialog
-        .showOpenDialog({
-            properties: ['openDirectory'],
-        })
-        .then((result) => {
-            if (!result.canceled) {
-                const filePath = result.filePaths[0] + '/奇门盘局记录.json';
-                fs.writeFile(filePath, data, (err) => {
-                    if (err) {
-                        e.reply('error', err);
-                    }
-                });
-            }
-        })
-        .catch((err) => {
+  data = JSON.stringify(data);
+  dialog
+    .showOpenDialog({
+      properties: ['openDirectory'],
+    })
+    .then((result) => {
+      if (!result.canceled) {
+        const filePath = result.filePaths[0] + '/奇门盘局记录.json';
+        fs.writeFile(filePath, data, (err) => {
+          if (err) {
             e.reply('error', err);
+          }
         });
+      }
+    })
+    .catch((err) => {
+      e.reply('error', err);
+    });
 });
 
 ipcMain.on('import data', (e, data) => {
-    dialog
-        .showOpenDialog({
-            properties: ['openFile'],
-            filters: [{ name: 'JSON Files', extensions: ['json'] }],
-        })
-        .then((result) => {
-            if (!result.canceled) {
-                const filePath = result.filePaths[0];
-                fs.readFile(filePath, 'utf-8', (err, data) => {
-                    if (err) {
-                        e.reply('error', err);
-                        return;
-                    }
-                    const contents = JSON.parse(data);
-                    let localData = [];
-                    for (let i = contents.length - 1; i >= 0; i--) {
-                        localData.unshift(contents[i]);
-                    }
-                    e.reply('import data to render process', localData);
-                });
-            }
-        })
-        .catch((err) => {
+  dialog
+    .showOpenDialog({
+      properties: ['openFile'],
+      filters: [{ name: 'JSON Files', extensions: ['json'] }],
+    })
+    .then((result) => {
+      if (!result.canceled) {
+        const filePath = result.filePaths[0];
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+          if (err) {
             e.reply('error', err);
+            return;
+          }
+          const contents = JSON.parse(data);
+          let localData = [];
+          for (let i = contents.length - 1; i >= 0; i--) {
+            localData.unshift(contents[i]);
+          }
+          e.reply('import data to render process', localData);
         });
+      }
+    })
+    .catch((err) => {
+      e.reply('error', err);
+    });
 });
--- dev/public/js/home_useDifferentScripts.js
+++ dev/public/js/home_useDifferentScripts.js
@@ -1,17 +1,17 @@
 document.getElementById('home-paipanfangshi').addEventListener('change', () => {
-    choosePaiPanScript();
+  choosePaiPanScript();
 });
 
 function choosePaiPanScript() {
-    const paipanIpc = document.createElement('script');
-    paipanIpc.type = 'module';
-    if (document.getElementById('home-paipanfangshi').value == '飞盘') {
-        document.getElementById('jushu').checked = true;
-        document.getElementById('shichen').checked = false;
-    } else if (document.getElementById('home-paipanfangshi').value == '转盘') {
-        document.getElementById('jushu').checked = false;
-        document.getElementById('shichen').checked = true;
-    }
+  const paipanIpc = document.createElement('script');
+  paipanIpc.type = 'module';
+  if (document.getElementById('home-paipanfangshi').value == '飞盘') {
+    document.getElementById('jushu').checked = true;
+    document.getElementById('shichen').checked = false;
+  } else if (document.getElementById('home-paipanfangshi').value == '转盘') {
+    document.getElementById('jushu').checked = false;
+    document.getElementById('shichen').checked = true;
+  }
 }
 
 choosePaiPanScript();
